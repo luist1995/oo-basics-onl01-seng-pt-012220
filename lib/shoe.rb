0e2132558b757ae06117fd817f920b93314df581
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-07 03:55:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :color, :size, :material, :condition = new
+  attr_accessor :color, :size, :material, :condition = "new"
  
   def initialize(brand)
     @brand = brand 
