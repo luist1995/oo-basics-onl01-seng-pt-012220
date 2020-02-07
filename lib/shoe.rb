b08146f1cfdd03d4349111c0c99cf7a45fb6f623
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-07 03:50:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :color, :, :genre
  
   def initialize(brand)
     @brand = brand 
   end
  
   def brand
-    @title
+    @brand
   end
  
   def turn_page
